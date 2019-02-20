fd5d76a64362f42b192aa8aa8936c8ebff177caa
Jan Niklas Hasse <jhasse@bixense.com>
2019-02-20 15:57:09 +0100

    Add sanity checks to prevent stuff like `rm++ -r /home`

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,13 @@ bool recursive = false;
 bool force = false;
 int exitcode = 0;
 
+bool askYes() {
+	std::string input;
+	std::getline(std::cin, input);
+	boost::algorithm::to_lower(input);
+	return input == "y" or input == "yes";
+}
+
 void remove(const std::string& filename, fs::path path) {
 	const auto error = [&filename](const std::string& msg) {
 		std::cerr << binaryName << ": cannot remove '" << filename << "': " << msg << "\n";
@@ -27,16 +34,36 @@ void remove(const std::string& filename, fs::path path) {
 	}
 	if (!force and (fs::status(path).permissions() & fs::perms::owner_write) == fs::perms::none) {
 		std::cout << binaryName << ": remove write-protected file '" << filename << "'? ";
-		std::string input;
-		std::getline(std::cin, input);
-		boost::algorithm::to_lower(input);
-		if (input != "y" && input != "yes") {
+		if (!askYes()) {
 			return;
 		}
 	}
 	if (!fs::remove(path)) {
 		error("No such file or directory");
 	}
+}
+
+// See https://stackoverflow.com/a/15549954/647898
+bool pathContainsFile(fs::path dir, fs::path file) {
+	// If dir ends with "/" and isn't the root directory, then the final component returned by
+	// iterators will include "." and will interfere with the std::equal check below, so we strip it
+	// before proceeding.
+	if (dir.filename() == ".") {
+		dir.remove_filename();
+	}
+	// We're also not interested in the file's name.
+	assert(file.has_filename());
+	file.remove_filename();
+
+	// If dir has more components than file, then file can't possibly reside in dir.
+	auto dir_len = std::distance(dir.begin(), dir.end());
+	auto file_len = std::distance(file.begin(), file.end());
+	if (dir_len > file_len) {
+		return false;
+	}
+	// This stops checking when it reaches dir.end(), so it's OK if file has more directory
+	// components afterward. They won't be checked.
+	return std::equal(dir.begin(), dir.end(), file.begin());
 }
 
 int main(int argc, char** argv) {
@@ -62,7 +89,7 @@ int main(int argc, char** argv) {
 	}
 
 	if (help) {
-		cli.parse(clara::Args{""}); // empty exeName so that Clara doesn't print usage information
+		cli.parse(clara::Args{ "" }); // empty exeName so that Clara doesn't print usage information
 		std::cout << "Usage: " << binaryName
 		          << " [OPTION]... [FILE]...\nRemove (unlink) the FILE(s).\n\n"
 		          << cli << "\n";
@@ -76,12 +103,76 @@ int main(int argc, char** argv) {
 		             "permitted by law.\n";
 		return 0;
 	}
+	const auto cwd = fs::current_path();
+	fs::path home;
+#ifdef _WIN32
+	if (const auto envHome = std::getenv("USERPROFILE")) {
+#else
+	if (const auto envHome = std::getenv("HOME")) {
+#endif
+		home = fs::canonical(envHome);
+#ifdef _WIN32
+	} else if (const auto envUser = std::getenv("USERNAME")) {
+		home = fs::path("C:/Users") / fs::path(envUser);
+#else
+	} else if (const auto envUser = std::getenv("USER")) {
+		home = fs::path("/home") / fs::path(envUser);
+#endif
+	}
+
+	// Warn when we're getting passed 2 folders and 2 files or more. This helps to prevent an
+	// accidental `rm++ -rf *` in big directories.
+	if (files.size() > 3) {
+		size_t numberOfFolders = 0;
+		for (const auto filename : files) {
+			if (fs::is_directory(filename)) {
+				++numberOfFolders;
+			}
+		}
+		const size_t numberOfFiles = files.size() - numberOfFolders;
+		if (numberOfFiles > 1 and numberOfFolders > 1) {
+			std::cout << "Delete " << numberOfFolders << " folders and " << numberOfFiles
+			          << " files? ";
+			if (!askYes()) {
+				return 1;
+			}
+		}
+	}
+
 	for (const std::string& filename : files) {
 		try {
+			fs::path path(filename);
+			if (recursive) {
+				const auto canonicalPath = fs::canonical(path);
+				if (canonicalPath == home) {
+					std::cout << "You're trying to delete your home directory. Continue? ";
+					if (!askYes()) {
+						continue;
+					}
+				} else if (pathContainsFile(canonicalPath, home)) {
+					std::cout << "You're trying to delete a path which is above your home "
+					             "directory. Continue? ";
+					if (!askYes()) {
+						continue;
+					}
+				} else if (canonicalPath == cwd) {
+					std::cout
+					    << "You're trying to delete your current working directory. Continue? ";
+					if (!askYes()) {
+						continue;
+					}
+				} else if (pathContainsFile(canonicalPath, cwd)) {
+					std::cout << "You're trying to delete a path which is above your current "
+					             "working directory. Continue? ";
+					if (!askYes()) {
+						continue;
+					}
+				}
+			}
 			if (recursive and force) {
-				fs::remove_all(fs::path(filename));
+				fs::remove_all(path);
 			} else {
-				remove(filename, fs::path(filename));
+				remove(filename, path);
 			}
 		} catch (fs::filesystem_error& err) {
 			std::cerr << binaryName << ": cannot remove '" << err.path1().string()
